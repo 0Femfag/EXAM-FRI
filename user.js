b406c0e473327d8ef069a1aa133e5957d85623d1
0Femfag <Femimane1@gmail.com>
2024-08-30 20:34:51 +0100

    Add files via upload

--- user.js
+++ user.js
@@ -1,38 +1,38 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      enum: ["Male", "Female"],
-    },
-    age: {
-      type: Number,
-    },
-    creatorId: {
-      type: mongoose.Types.ObjectId,
-      required: true,
-    },
-    details: {
-      address: String,
-      postcode: Number,
-    },
-  },
-  { timestamps: true }
-);
+const userModel = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-const userModel = mongoose.model("LUMINARY", userSchema);
-module.exports = userModel;
+const createnewUser = async (req, res) => {
+  const { password, ...others } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  console.log(hashedPassword);
+  const newUser = new userModel({ password: hashedPassword, ...others });
+  try {
+    await newUser.save();
+    res.json({ message: "user created successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ message: "user already exist" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const findUser = await userModel.findOne({ email });
+    if (!findUser) {
+      return res.json({ message: "user not found" });
+    }
+    const verify = bcrypt.compareSync(password, findUser.password);
+    console.log(verify);
+    if (!verify) {
+      return res.json({ message: "pass doesn't match" });
+    }
+    res.json(findUser);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ message: error.message });
+  }
+};
+
+module.exports = { createnewUser, loginUser };
